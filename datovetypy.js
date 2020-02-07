80a060a438b3494008f0acc2f04ddefbdc347712
iVanossx <silversix.noob@gmail.com>
2020-02-07 17:39:56 +0100

    Datové typy přidany

---
+++ datovetypy.js
@@ -0,0 +1,16 @@
+class Zakaznik {
+    id;             //jednoznačný identifikátor
+    email;
+    telCislo;
+}
+
+class Kadernik {
+    id;             //jednoznačný identifikátor
+    nazev;
+    email;
+    telCislo;
+    poloha;         //poloha vyjádřená jako geohash např. http://geohash.gofreerange.com/
+    adresa;         // textově adresa
+    cena;
+    heslo;
+}
