3165540ef6a38c0c56fe613f5d8d0fdf3fedc500
lra <lra@r73.info>
2020-03-22 17:59:55 +0100

    server

--- update-from-repo.sh
+++ update-from-repo.sh
@@ -3,8 +3,8 @@
 cd $(dirname $0)
 git pull 
 
-echo -n '{"rev":"' > rev.js
-echo -n "$(git rev-parse HEAD)" >>rev.js
+echo -n '{"rev":"' > rev.json
+echo -n "$(git rev-parse HEAD)" >>rev.json
 echo -n '"}'>>rev.json
 
 
